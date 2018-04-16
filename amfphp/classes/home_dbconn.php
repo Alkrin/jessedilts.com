e58821103288e18046dd4cae45c2a3fc5c5341bb
Alkrin <alkrin@gmail.com>
2018-04-15 22:05:31 -0700

    Updated MySQL db password

--- amfphp/classes/home_dbconn.php
+++ amfphp/classes/home_dbconn.php
@@ -5,7 +5,7 @@ $DB_SERVER = $_ENV['DATABASE_SERVER'];
 //database login name
 $DB_USER = 'db131986';
 //database login password
-$DB_PASS = 'eestimaa0';
+$DB_PASS = 'Eestimaa0+';
 
 //database name
 $DB_DATABASE = 'db131986_homepage';
