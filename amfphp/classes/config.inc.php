746012d7abb6182d38b0e96e01eb79e9b69a7c29
Alkrin <alkrin@gmail.com>
2018-04-15 22:21:41 -0700

    Update DB server address

--- amfphp/classes/config.inc.php
+++ amfphp/classes/config.inc.php
@@ -1,12 +1,12 @@
 <?php
 //database server
-define('DB_SERVER', "ww26.empiredatatech.com");
+define('DB_SERVER', "internal-db.s131986.gridserver.com");
 
 //database login name
-define('DB_USER', "jessed_lugumees");
+define('DB_USER', "db131986");
 //database login password
-define('DB_PASS', "jessed_eestimaa0");
+define('DB_PASS', "Eestimaa0+");
 
 //database name
-define('DB_DATABASE', "jessed_homepage");
+define('DB_DATABASE', "db131986_homepage");
 ?>
